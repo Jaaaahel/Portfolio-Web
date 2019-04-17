51403752957fee0cc67d80358b17b221b365bbc6
Jeremiah <jahelrelova@outlook.com>
2019-04-18 00:50:32 +0800

    Update

--- js/jquery.js
+++ js/jquery.js
@@ -45,38 +45,55 @@ $(function(){
 	);
 
 	$('.faces-project').hover(function () {
-	    $(".faces-project-tags").fadeIn(100);
+	    $(".faces-tags").fadeIn(100);
+	    $(".faces-logo").fadeOut(150, function(){
+	    	$(this).attr("src", "assets/faces-img.png").fadeIn(150);
+	    })
 	},	
 		function () {
 			$(".tags").fadeOut(50);
+			$(".faces-logo").fadeOut(150, function(){
+	    	$(this).attr("src", "assets/faces-logo.png").fadeIn(150);
+	    	})
 		}
 	);
 
 	$('.esapp-project').hover(function () {
-	    $(".esapp-project-tags").fadeIn(100);
+	    $(".esapp-tags").fadeIn(100);
+	    $(".esapp-logo").fadeOut(150, function(){
+	    	$(this).attr("src", "assets/esapp-img.png").fadeIn(150);
+	    })
+
 	},	
 		function () {
 			$(".tags").fadeOut(50);
+			$(".esapp-logo").fadeOut(150, function(){
+	    	$(this).attr("src", "assets/esweb-logo.png").fadeIn(150);
+	    	})
 		}
 	);
 
 	$('.dta-project').hover(function () {
-	    $(".dta-project-tags").fadeIn(100);
+	    $(".dta-tags").fadeIn(100);
+	     $(".dta-logo").fadeOut(150, function(){
+	    	$(this).attr("src", "assets/dta-img.png").fadeIn(150);
+	    })
 	},	
 		function () {
 			$(".tags").fadeOut(50);
+			$(".dta-logo").fadeOut(150, function(){
+	    	$(this).attr("src", "assets/dta-logo.png").fadeIn(150);
+	    	})
 		}
 	);
 
 	$('.vt-project').hover(function () {
-	    $(".vt-project-tags").fadeIn(100);
+	    $(".vt-tags").fadeIn(100);
 	},	
 		function () {
 			$(".tags").fadeOut(50);
 		}
 	);
 
-
 	// $(".tags").css('border', '4px solid red');
-
 });
