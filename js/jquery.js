224eda2131250dc18af50ecaf07978e20f3e49be
Jeremiah <jahelrelova@outlook.com>
2019-04-24 01:33:54 +0800

    Remake

--- js/jquery.js
+++ js/jquery.js
@@ -1,99 +1,29 @@
 $(function(){
+	$('.project').hover(function () {
+		var screenshot = $(this).find('.screenshot');
+		var logo = $(this).find('.logo');
 
-	$(".tags").hide();
+		$(this).find('.descrip').css('display','none');
+		$(this).find('.tags').addClass('is-inline-block');
+		$(this).find('.tags').css('display','none');
+		$(this).find('.tags').animate({ opacity: 1 }, 100);
 
-	$('.portfolio-project').hover(function () {
-	    $(".portfolio-tags").fadeIn(100);
-	    $(".portfolio-logo").fadeOut(150, function(){
-	    	$(this).attr("src", "assets/portfolio-img.png").fadeIn(150);
-	    })
-	},	
-		function () {
-			$(".tags").fadeOut(50);
-			$(".portfolio-logo").fadeOut(150, function(){
-	    	$(this).attr("src", "assets/portfolio-logo.png").fadeIn(150);
-	    	})
-		}
-	);
+		logo.animate({ opacity: 0 }, 100, function () {
+			screenshot.animate({ opacity: 1 }, 100);
+		});
+	}, function () {
+		var screenshot = $(this).find('.screenshot');
+		var logo = $(this).find('.logo');
 
-	$('.va-project').hover(function () {
-	    $(".va-tags").fadeIn(100);
-	    $(".va-logo").fadeOut(150, function(){
-	    	$(this).attr("src", "assets/va-img.png").fadeIn(150);
-	    })
-	},	
-		function () {
-			$(".tags").fadeOut(50);
-			$(".va-logo").fadeOut(150, function(){
-	    	$(this).attr("src", "assets/va-logo.png").fadeIn(150);
-	    	})
-		}
-	);
+		$(this).find('.descrip').css('display','block');
+		$(this).find('.tags').animate({ opacity: 0 }, 100);
+		$(this).find('.tags').removeClass('is-inline-block');
+		$(this).find('.tags').css('display','none');
 
-	$('.esweb-project').hover(function () {
-	    $(".esweb-tags").fadeIn(100);
-	    $(".esweb-logo").fadeOut(150, function(){
-	    	$(this).attr("src", "assets/esweb-img.png").fadeIn(150);
-	    })
-	},	
-		function () {
-			$(".tags").fadeOut(50);
-			$(".esweb-logo").fadeOut(150, function(){
-	    	$(this).attr("src", "assets/esweb-logo.png").fadeIn(150);
-	    	})
-		}
-	);
-
-	$('.faces-project').hover(function () {
-	    $(".faces-tags").fadeIn(100);
-	    $(".faces-logo").fadeOut(150, function(){
-	    	$(this).attr("src", "assets/faces-img.png").fadeIn(150);
-	    })
-	},	
-		function () {
-			$(".tags").fadeOut(50);
-			$(".faces-logo").fadeOut(150, function(){
-	    	$(this).attr("src", "assets/faces-logo.png").fadeIn(150);
-	    	})
-		}
-	);
-
-	$('.esapp-project').hover(function () {
-	    $(".esapp-tags").fadeIn(100);
-	    $(".esapp-logo").fadeOut(150, function(){
-	    	$(this).attr("src", "assets/esapp-img.png").fadeIn(150);
-	    })
-
-	},	
-		function () {
-			$(".tags").fadeOut(50);
-			$(".esapp-logo").fadeOut(150, function(){
-	    	$(this).attr("src", "assets/esweb-logo.png").fadeIn(150);
-	    	})
-		}
-	);
-
-	$('.dta-project').hover(function () {
-	    $(".dta-tags").fadeIn(100);
-	     $(".dta-logo").fadeOut(150, function(){
-	    	$(this).attr("src", "assets/dta-img.png").fadeIn(150);
-	    })
-	},	
-		function () {
-			$(".tags").fadeOut(50);
-			$(".dta-logo").fadeOut(150, function(){
-	    	$(this).attr("src", "assets/dta-logo.png").fadeIn(150);
-	    	})
-		}
-	);
-
-	$('.vt-project').hover(function () {
-	    $(".vt-tags").fadeIn(100);
-	},	
-		function () {
-			$(".tags").fadeOut(50);
-		}
-	);
+		screenshot.animate({ opacity: 0 }, 100, function () {
+			logo.animate({ opacity: 1 }, 100);
+		})
+	});
 
 	// $(".tags").css('border', '4px solid red');
 });
